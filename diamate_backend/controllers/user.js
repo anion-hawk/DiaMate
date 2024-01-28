3c54f2e048a03f1acbd55c5e68e052df54802f4e
anion-hawk <hoque.anindya2@gmail.com>
2024-01-28 20:15:46 +0600

    Complete profile endpoint added

--- diamate_backend/controllers/user.js
+++ diamate_backend/controllers/user.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const userRepository = require('../repository/user');
+const patientRepository = require('../repository/patient');
 const jwt = require('jsonwebtoken');
 
 async function loginHandler(queryResult, res) {
@@ -75,10 +76,85 @@ async function getUserById(req, res) {
     return;
 }
 
+async function completePatientProfile(req, res) {
+    const { id } = req.user;
+    const patientQuery = await patientRepository.isProfileComplete(id);
+    if (!patientQuery.success) {
+        res.status(500).json({ error: "Internal server error" });
+        return;
+    }
+    if (patientQuery.isComplete) {
+        res.status(301).redirect('/user/profile');
+        return;
+    }
+    const { type, diagnosis_date, diseases } = req.body;
+
+    let diabetes_type;
+    if (type.toLowerCase() === "type 1") {
+        diabetes_type = 1;
+    } else if (type.toLowerCase() === "type 2") {
+        diabetes_type = 2;
+    } else {
+        diabetes_type = 0;
+    }
+
+    const disease_keys = diseases.map(disease => disease.toLowerCase().replace(/ /g, '_'));
+
+    let queryBody = {
+        id,
+        diabetes_type,
+        diagnosis_date,
+        coeliac_disease: false,
+        thyroid_disease: false,
+        hypertension: false,
+        pcos: false,
+        cataract: false,
+        diabetes_insipidus: false,
+        insulin_resistance: false,
+    };
+    const keys = Object.keys(queryBody);
+    const hasInvalidKey = disease_keys.some(key => !keys.includes(key));
+    if (hasInvalidKey) {
+        res.status(400).json({ error: "Invalid key in diseases list" });
+        return;
+    }
+    disease_keys.forEach(key => { queryBody[key] = true });
+
+    const result = await patientRepository.completeProfile(queryBody);
+    if (!result.success) {
+        res.status(500).json({ error: "Internal server error: failed adding details" });
+        return;
+    }
+    res.status(201).json({ message: "Patient details added successfully" });
+}
+
+async function completeDoctorProfile(req, res) {
+    res.status(404).json({ error: "Not available" });
+}
+
+async function completeProfile(req, res) {
+    const { role } = req.user;
+
+    switch (role) {
+        case 1:
+            await completePatientProfile(req, res);
+            break;
+        case 2:
+            await completeDoctorProfile(req, res);
+            break;
+        default:
+            res.status(403).json({ error: "Forbidden" });
+            break;;
+    }
+    return;
+
+}
+
 module.exports = {
     login,
     logout,
     register,
     getSelfProfile,
-    getUserById
+    getUserById,
+    completeProfile
 };
--- diamate_backend/routes/user.js
+++ diamate_backend/routes/user.js
@@ -5,5 +5,6 @@ const verifyToken = require('../controllers/middleware/verify_token');
 
 router.route('/profile').get(verifyToken, userController.getSelfProfile);
 router.route('/:id').get(verifyToken, userController.getUserById);
+router.route('/complete-profile').post(verifyToken, userController.completeProfile);
 
 module.exports = router;
