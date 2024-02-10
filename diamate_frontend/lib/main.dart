76d7817047e60328dfeaf1bf70c649c6b5b0962c
anion-hawk <hoque.anindya2@gmail.com>
2024-02-11 03:29:08 +0600

    added firebase

--- diamate_frontend/lib/main.dart
+++ diamate_frontend/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'core/app_export.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as dotenv;
 import 'package:diamate_frontend/config.dart';
@@ -9,6 +11,9 @@ var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
 void main() async {
   // await dotenv.dotenv.load();
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
