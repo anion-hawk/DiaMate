5502bda8b9a0bd8826a271ea0d8666b8788ad537
MajishaJahan <majishajahandisha@gmail.com>
2024-02-19 03:48:10 +0600

    edit profile ui done

--- diamate_frontend/lib/presentation/edit_profile_screen/edit_profile_screen.dart
+++ diamate_frontend/lib/presentation/edit_profile_screen/edit_profile_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:diamate_frontend/core/app_export.dart';
 import 'package:diamate_frontend/widgets/custom_drop_down.dart';
-//import 'package:diamate_frontend/widgets/custom_elevated_button.dart';
+import 'package:diamate_frontend/widgets/coeliacdiseasechipview_item_widget.dart';
 import 'package:diamate_frontend/widgets/form_text.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer';
@@ -24,17 +24,29 @@ class EditProfileScreen extends StatefulWidget {
 }
 
 class _EditProfileScreen extends State<EditProfileScreen> {
-  final nameController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    // Fetch data from the backend when the widget is created
+    //getUserProfile();
+  }
+
+  final nameController = TextEditingController(text: "abcd");
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   final confirmPasswordController = TextEditingController();
   final dobController = TextEditingController();
   String selectedRole = "";
+  TextEditingController dateController = TextEditingController();
+  List<String> selectedDiseases = [];
+  String dType = "";
+  TextEditingController ec1Controller = TextEditingController();
+  TextEditingController ec2Controller = TextEditingController();
+  TextEditingController ec3Controller = TextEditingController();
 
   List<String> dropdownItemList = [
-    "Item One",
-    "Item Two",
-    "Item Three",
+    "Type 1",
+    "Type 2",
   ];
 
   List<String> dropdownItemList1 = [
@@ -53,87 +65,237 @@ class _EditProfileScreen extends State<EditProfileScreen> {
   final ImagePicker _picker = ImagePicker();
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
         body: SafeArea(
             child: Center(
                 child: Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-      child: ListView(children: [
-        const Center(
-          child: Text(
-            "Edit Profile",
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-          ),
-        ),
-        imageProfile(),
-        const SizedBox(height: 20),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 40),
+                    child: SingleChildScrollView(
+                      // Wrap your content with SingleChildScrollView
+                      child: Column(children: [
+                        const Center(
+                          child: Text(
+                            "Edit Profile",
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        imageProfile(),
+                        const SizedBox(height: 20),
 
-        // name
-        CustomTextFormField(
-            controller: nameController,
-            hintText: "Name",
-            obscureText: false,
-            icon: const Icon(Icons.person)),
-        const SizedBox(height: 20),
+                        // name
+                        const Row(
+                          children: [
+                            Text(
+                              "Name :",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            // Add additional widgets as needed
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        CustomTextFormField(
+                            controller: nameController,
+                            hintText: "Name",
+                            obscureText: false,
+                            icon: const Icon(Icons.person)),
+                        const SizedBox(height: 20),
 
-        // email
-        CustomTextFormField(
-            controller: emailController,
-            hintText: "Email",
-            obscureText: false,
-            icon: const Icon(Icons.mail)),
-        const SizedBox(height: 20),
+                        // email
+                        const Row(
+                          children: [
+                            Text(
+                              "Email :",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            // Add additional widgets as needed
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        CustomTextFormField(
+                            controller: emailController,
+                            hintText: "Email",
+                            obscureText: false,
+                            icon: const Icon(Icons.mail)),
+                        const SizedBox(height: 20),
 
-        // password
-        CustomTextFormField(
-            controller: passwordController,
-            hintText: "Password",
-            obscureText: true,
-            icon: const Icon(Icons.lock)),
-        const SizedBox(height: 20),
+                        // password
+                        const Row(
+                          children: [
+                            Text(
+                              "Password :",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            // Add additional widgets as needed
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        CustomTextFormField(
+                            controller: passwordController,
+                            hintText: "Password",
+                            obscureText: true,
+                            icon: const Icon(Icons.lock)),
+                        const SizedBox(height: 20),
 
-        // confirm password
-        CustomTextFormField(
-            controller: confirmPasswordController,
-            hintText: "Confirm Password",
-            obscureText: true,
-            icon: const Icon(Icons.lock),
-            validator: (value) {
-              if (value != passwordController.text) {
-                return "Passwords do not match";
-              }
-              return null;
-            }),
-        const SizedBox(height: 20),
+                        // confirm password
+                        const Row(
+                          children: [
+                            Text(
+                              "Confirm Password :",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            // Add additional widgets as needed
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        CustomTextFormField(
+                            controller: confirmPasswordController,
+                            hintText: "Confirm Password",
+                            obscureText: true,
+                            icon: const Icon(Icons.lock),
+                            validator: (value) {
+                              if (value != passwordController.text) {
+                                return "Passwords do not match";
+                              }
+                              return null;
+                            }),
+                        const SizedBox(height: 20),
 
-        // dob
-        InkWell(
-          onTap: () {
-            _selectDate(context);
-          },
-          child: AbsorbPointer(
-            child: CustomTextFormField(
-                controller: dobController,
-                hintText: "Date of Birth",
-                obscureText: false,
-                icon: const Icon(Icons.calendar_today_rounded),
-                textInputType: TextInputType.datetime),
-          ),
-        ),
+                        // dob
+                        const Row(
+                          children: [
+                            Text(
+                              "Date of Birth :",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            // Add additional widgets as needed
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        InkWell(
+                          onTap: () {
+                            _selectDate(context);
+                          },
+                          child: AbsorbPointer(
+                            child: CustomTextFormField(
+                                controller: dobController,
+                                hintText: "Date of Birth",
+                                obscureText: false,
+                                icon: const Icon(Icons.calendar_today_rounded),
+                                textInputType: TextInputType.datetime),
+                          ),
+                        ),
 
-        const SizedBox(height: 20),
+                        const SizedBox(height: 20),
+                        const Row(
+                          children: [
+                            Text(
+                              "Type of Diabetes :",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            // Add additional widgets as needed
+                          ],
+                        ),
 
-        // login button
-        CustomElevatedButton(
-            text: "Register",
-            onPressed: () {
-              // register();
-            }),
-        // register button
-        const SizedBox(height: 40),
-        const Center(child: Text("Already have an account?")),
-      ]),
-    ))));
+                        CustomDropDown(
+                            items: dropdownItemList,
+                            hintText: "Type",
+                            icon: const Icon(Icons.arrow_drop_down),
+                            onChanged: (value) {
+                              dType = value;
+                            }),
+                        const SizedBox(height: 20),
+
+                        // date of diagnosis
+                        const Row(
+                          children: [
+                            Text(
+                              "Date of Diagnosis :",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            // Add additional widgets as needed
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        InkWell(
+                          onTap: () {
+                            _selectDiagnosisDate(context);
+                          },
+                          child: AbsorbPointer(
+                            child: CustomTextFormField(
+                                controller: dateController,
+                                hintText: "Date of Diagnosis",
+                                obscureText: false,
+                                icon: const Icon(Icons.calendar_today_rounded),
+                                textInputType: TextInputType.datetime),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+
+                        // other disieses
+                        const Row(
+                          children: [
+                            Text(
+                              "Other Diseases :",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            // Add additional widgets as needed
+                          ],
+                        ),
+
+                        const SizedBox(height: 10),
+                        _buildCoeliacDiseaseChipView(context),
+                        //emergency contacts
+                        const SizedBox(height: 20),
+                        const SizedBox(height: 50),
+                        
+                        const Row(
+                          children: [
+                            Text(
+                              "Emergency Contact :",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            // Add additional widgets as needed
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        CustomTextFormField(
+                            controller: ec1Controller,
+                            hintText: "Emergency Contact 1",
+                            obscureText: false,
+                            icon: const Icon(Icons.phone)),
+                        const SizedBox(height: 10),
+                        CustomTextFormField(
+                            controller: ec2Controller,
+                            hintText: "Emergency Contact 2",
+                            obscureText: false,
+                            icon: const Icon(Icons.phone)),
+                        const SizedBox(height: 10),
+                        CustomTextFormField(
+                            controller: ec3Controller,
+                            hintText: "Emergency Contact 3",
+                            obscureText: false,
+                            icon: const Icon(Icons.phone)),
+                        const SizedBox(height: 20),
+
+                        // login button
+                        CustomElevatedButton(
+                            text: "Save Changes",
+                            onPressed: () {
+                              // register();
+                            }),
+                      ]),
+                    )))));
   }
 
   void _selectDate(context) {
@@ -146,6 +308,20 @@ class _EditProfileScreen extends State<EditProfileScreen> {
         .then((value) {
       if (value != null) {
         dobController.text = DateFormat('yyyy-MM-dd').format(value);
+      }
+    });
+  }
+
+  void _selectDiagnosisDate(context) {
+    showDatePicker(
+            context: context,
+            initialDate:
+                DateTime.now().subtract(const Duration(days: 18 * 365)),
+            firstDate: DateTime(1900),
+            lastDate: DateTime.now())
+        .then((value) {
+      if (value != null) {
+        dateController.text = DateFormat('yyyy-MM-dd').format(value);
       }
     });
   }
@@ -184,9 +360,11 @@ class _EditProfileScreen extends State<EditProfileScreen> {
                     ),
                   ),
                   style: ButtonStyle(
-                   backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 162, 218, 174)),
-                   minimumSize: MaterialStateProperty.all<Size>(Size(150.0, 50.0)), // Set your desired background color
-                   ),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        Color.fromARGB(255, 162, 218, 174)),
+                    minimumSize: MaterialStateProperty.all<Size>(
+                        Size(150.0, 50.0)), // Set your desired background color
+                  ),
                 ),
                 SizedBox(width: 18.0), // Add horizontal space
                 ElevatedButton.icon(
@@ -201,9 +379,11 @@ class _EditProfileScreen extends State<EditProfileScreen> {
                     ),
                   ),
                   style: ButtonStyle(
-                   backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 162, 218, 174)),
-                   minimumSize: MaterialStateProperty.all<Size>(Size(150.0, 50.0)), // Set your desired background color
-                   ),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        Color.fromARGB(255, 162, 218, 174)),
+                    minimumSize: MaterialStateProperty.all<Size>(
+                        Size(150.0, 50.0)), // Set your desired background color
+                  ),
                 ),
               ],
             )
@@ -262,5 +442,37 @@ class _EditProfileScreen extends State<EditProfileScreen> {
         ),
       ]),
     );
+  }
+
+  //   /// Section Widget
+  /// Section Widget
+  Widget _buildCoeliacDiseaseChipView(BuildContext context) {
+    return DiseaseWidget(
+      onOptionsChanged: (options) {
+        print(options.runtimeType);
+        // Receive the selected options from DiseaseWidget
+        selectedDiseases = options;
+        print("Selected Diseases: $selectedDiseases ");
+        print(dateController.text);
+        print(dType);
+      },
+    );
+  }
+
+  void getUserProfile() async {
+    try {
+      // Replace 'your_api_endpoint' with the actual API endpoint for fetching user profile
+      var response = await Requests.get('your_api_endpoint');
+
+      if (response.success) {
+        // The response body contains the user profile information
+        print('User Profile: ${response.json()}');
+      } else {
+        print(
+            'Failed to fetch user profile. Status Code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error during GET request: $e');
+    }
   }
 }
