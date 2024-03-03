3eb03ac421bf1718ffefa7957ba0c945bfc5d117
anion-hawk <hoque.anindya2@gmail.com>
2024-03-04 03:36:02 +0600

    complete patient profile done

--- diamate_frontend/lib/config.dart
+++ diamate_frontend/lib/config.dart
@@ -1,12 +1,12 @@
-String baseUrl = "http://192.168.250.46:3000";
+String baseUrl = "http://192.168.47.13:3000";
 
 final url_register = '$baseUrl/auth/register';
 final url_cookieCheck = '$baseUrl/user/test';
 final url_selfProfile = '$baseUrl/user/profile';
 final url_profileStatus = '$baseUrl/user/status';
+final url_complete_profile = '$baseUrl/user/complete-profile';
 
 final show_post = "$baseUrl/forum/show";
-final compUserProf = "$baseUrl/user/complete-profile";
 final own_post = "$baseUrl/forum/post";
 final forum = "$baseUrl/forum/posts";
 final selffollowdetails = baseUrl + "/user/self-follow-details";
